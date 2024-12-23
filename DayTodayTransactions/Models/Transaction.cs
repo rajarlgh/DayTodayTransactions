929e74c63df45b04e4bd79a067ff0aa08f5c57f2
Rajaram Lakshmanan <rajaram.lakshmanan@siemens-healthineers.com>
2024-12-23 11:58:59 +0530

    Created the transaction project

---
+++ DayTodayTransactions/Models/Transaction.cs
@@ -0,0 +1,13 @@
+﻿namespace DayTodayTransactions.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public required string Reason { get; set; }
+        public required string Type { get; set; } // "Income" or "Expense"
+        public required string Category { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+}
---
+++ DayTodayTransactions/ViewModels/TransactionViewModel.cs
@@ -0,0 +1,46 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using DayTodayTransactions.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DayTodayTransactions.ViewModels
+{
+    public partial class TransactionViewModel : ObservableObject
+    {
+        //private readonly ITransactionService _transactionService;
+
+        //public TransactionViewModel(ITransactionService transactionService)
+        //{
+        //    _transactionService = transactionService;
+        //}
+
+        [ObservableProperty]
+        private decimal amount;
+
+        [ObservableProperty]
+        private string reason;
+
+        [ObservableProperty]
+        private string type; // Income or Expense
+
+        [ObservableProperty]
+        private string category;
+
+        [ObservableProperty]
+        private DateTime date = DateTime.Now;
+
+        public async Task AddTransactionAsync()
+        {
+            var transaction = new Transaction
+            {
+                Amount = Amount,
+                Reason = Reason,
+                Type = Type,
+                Category = Category,
+                Date = Date
+            };
+
+            //await _transactionService.AddTransactionAsync(transaction);
+        }
+    }
+
+}
