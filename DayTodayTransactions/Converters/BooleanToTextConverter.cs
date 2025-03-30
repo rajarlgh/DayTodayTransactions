4fc7e45ce4c4b12247f772a36edc5f28bc16d1cc
Rajaram Lakshmanan <rajaram.lakshmanan@siemens-healthineers.com>
2025-03-30 11:38:28 +0530

    Unit test and fix done

---
+++ DayTodayTransactions/Converters/BooleanToTextConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayTodayTransactions.Converters
+{
+    public class BooleanToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isExpanded)
+            {
+                return isExpanded ? "Hide Filters" : "Show Filters";
+            }
+            return "Show Filters";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
